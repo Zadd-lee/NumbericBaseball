b6158436d4cd419638ad4e9a0886257bd39f4371
이자드 <kimminkyeong0418@gmail.com>
2024-10-21 15:25:06 +0900

    feat: 게임 기능 구현 완료

--- src/main/java/game/Game.java
+++ src/main/java/game/Game.java
@@ -1,7 +1,6 @@
 package game;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class Game {
 
@@ -9,40 +8,65 @@ public class Game {
     public static final String BALL = "Ball";
     public static final String OUT = "Out";
     int tryCnt;
-    int[] answer;
-    Map<String, Integer> score = new HashMap<>();
+    List<Integer> answer;
+    int digits;
+    Map<String, Integer> scoreBoard = new HashMap<>();
 
     public void start() {
-        //값 가져오기
+
+        System.out.println("숫자 야구 게임 시작!");
 
         //정답 생성
-        answer = new Answer(3).getAnswer();
+        answer = new Answer(digits).getAnswer();
+        System.out.println(answer);
         Parser parser = new Parser();
 
         do {
             //입력값 받기
-            int[] input = parser.getUserAnswer();
+            List<Integer> input = parser.getUserAnswer(digits);
+
             //입력값과 정답이 같은지 확인
             clearScore();
             getScore(input);
 
-        }while (score.get(STRIKE)==3);
+            tryCnt++;
 
+            //결과 값 출력
+            System.out.println("---------------------");
+            System.out.println("STRIKE :"+scoreBoard.get(STRIKE));
+            System.out.println("BALL :"+scoreBoard.get(BALL));
+            System.out.println("OUT :"+scoreBoard.get(OUT));
+            System.out.println("---------------------");
 
+        }while (scoreBoard.get(STRIKE)!=digits);
+        System.out.println();
+        System.out.println("축하합니다! 성공했습니다");
+        System.out.println("정답은 "+answer.toString()+" 입니다!");
+        System.out.println("이번 게임의 시도 횟수는 "+tryCnt+ "입니다.");
 
     }
 
-    private void getScore(int[] input) {
+    private void getScore(List<Integer> input) {
         //strike
-        for (int i = 0; i < input.length; i++) {
-            if (input[i] == answer[i]) {
-                score.put(STRIKE, score.get(STRIKE));
+        for (int i = 0; i < digits; i++) {
+            if (input.get(i) == answer.get(i)) {
+                scoreBoard.put(STRIKE, scoreBoard.get(STRIKE)+1);
             }
         }
 
         //ball
+        int ballCnt =0;
+        for (int i = 0; i < digits; i++) {
+            if (input.contains(answer.get(i))) {
+                ballCnt++;
+            }
+
+        }
+        //숫자가 같은 경우 중 자릿수가 같은 경우 제외
+        scoreBoard.put(BALL, ballCnt- scoreBoard.get(STRIKE));
 
         //out
+        scoreBoard.put(OUT, digits - scoreBoard.get(STRIKE) - scoreBoard.get(BALL));
     }
 
     public int getTryCnt() {
@@ -50,15 +74,17 @@ public class Game {
     }
 
     private void clearScore() {
-        score.clear();
-        score.put(STRIKE,0);
-        score.put(BALL, 0);
-        score.put(OUT, 0);
+        scoreBoard.clear();
+        scoreBoard.put(STRIKE,0);
+        scoreBoard.put(BALL, 0);
+        scoreBoard.put(OUT, 0);
     }
 
-    public Game() {
+    public Game(int digits) {
         //점수 초기화
         clearScore();
-        tryCnt=1;
+        this.tryCnt=0;
+        this.digits = digits;
+
     }
 }
