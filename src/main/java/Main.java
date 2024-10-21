3d1fe414fabfb8b0085a8c362853b72eff1a560e
이자드 <kimminkyeong0418@gmail.com>
2024-10-21 16:47:24 +0900

    feat: 자리수 설정 기능 추가

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,5 @@
 import game.Game;
+import game.Parser;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,16 +15,47 @@ public class Main {
         //게임 반복 실행
 
         Scanner sc = new Scanner(System.in);
+        String command;
+        int digits=3;
+        Parser parser = new Parser();
+
         do {
-            System.out.println("----------");
-
-            Game game = new Game(3);
-            game.start();
-            history.add(game.getTryCnt());
 
             System.out.println("----------");
-            System.out.println("계속 하시겠습니까?(Y/N중 입력)");
+            System.out.println("환영합니다! 원하시는 번호를 입력해주세요");
+            System.out.println("0. 자리수 설정 1. 게임 시작하기 2. 게임 기록 보기 3. 종료하기");
+            command = parser.getCommand();
 
-        } while (sc.nextLine().equals("Y"));
+
+            if (command.equals("0")) {
+                digits = parser.getDigits();
+                startGame(digits);
+            }
+            if (command.equals("1")) {
+                startGame(digits);
+            } else if (command.equals("2")) {
+                printHistory();
+            } else if (command.equals("3")) {
+                System.out.println("< 숫자 야구 게임을 종료합니다 >");
+            }
+
+
+        } while (!command.equals("3"));
+    }
+
+    private static void startGame(int digit) {
+        System.out.println("< 게임을 시작합니다. >");
+        Game game = new Game(digit);
+        game.start();
+        history.add(game.getTryCnt());
+    }
+
+    private static void printHistory() {
+        if (history.isEmpty()) {
+            System.out.println("기록이 없습니다");
+        }
+        for (int i = 0; i < history.size(); i++) {
+            System.out.println(i + "번째 게임: 시도 횟수 - " + history.get(i));
+        }
     }
 }
